1301adf41e354e2cc051b63bcd5596e3cd7b8a27
m1-k8s <m1-k8s@localhost.localdomain>
2023-02-07 05:07:23 -0500

    added install.sh

---
+++ centos7/install.sh
@@ -0,0 +1,83 @@
+#system update
+yum -y install epel-release
+yum -y update
+
+#SELINUX disable
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=disabled/' /etc/selinux/config
+
+#SWAP Disable
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+# RHEL/CentOS 7 have reported traffic issues being routed incorrectly due to iptables bypassed
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo modprobe br_netfilter
+sudo sysctl --system
+
+#Enable Firewall
+firewall-cmd --permanent --zone=public --add-port=6443/tcp
+firewall-cmd --permanent --zone=public --add-port=2379-2380/tcp
+firewall-cmd --permanent --zone=public --add-port=10250/tcp
+firewall-cmd --permanent --zone=public --add-port=10251/tcp
+firewall-cmd --permanent --zone=public --add-port=10252/tcp
+firewall-cmd --permanent --zone=public --add-port=10255/tcp
+firewall-cmd --permanent --zone=public --add-port=30000-32767/tcp
+firewall-cmd --permanent --zone=public --add-port=179/tcp
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-port=443/tcp
+firewall-cmd --permanent --zone=public --add-port=4443/tcp
+firewall-cmd --reload
+
+# local small dns & vagrant cannot parse and delivery shell code.
+echo "203.250.35.27 m1-k8s" >> /etc/hosts
+echo "203.250.33.67 n1-swdeep" >> /etc/hosts
+echo "203.250.32.219 n2-k8s" >> /etc/hosts
+
+
+#inser kubenetes repo
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+#install kube
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+#install containerd
+yum install yum-utils -y
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install containerd.io -y
+
+#daemon editing
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+
