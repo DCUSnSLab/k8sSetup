dff055e7eda7c8bf5d428e38c1db35207ed42536
soobinJeon <marsberry@cu.ac.kr>
2023-02-08 22:44:51 -0500

    updated server setup

--- centos7/reinstall.sh
+++ centos7/reinstall.sh
@@ -1,3 +1,22 @@
+#reset node
+sudo kubeadm reset
+
+sudo ip link delete cni0
+sudo ip link delete flannel.1
+
+#delete files
+sudo rm -rf /var/lib/cni/
+sudo rm -rf /var/lib/kubelet/*
+sudo rm -rf /var/lib/etcd/
+sudo rm -rf /run/flannel
+sudo rm -rf /etc/cni/
+sudo rm -rf /etc/kubernetes
+sudo rm -rf ~/.kube
+
+#delete packages
+sudo yum remove kubeadm kubectl kubelet kubernetes-cni kube*
+sudo yum autoremove
+
 #install kube
 yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo systemctl enable --now kubelet
