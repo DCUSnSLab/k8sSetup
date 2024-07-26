48953820033dfeb71ddb7364c2d44441cb0c458e
chan-a12 <ami159@cu.ac.kr>
2024-07-26 16:05:50 +0900

    add huge page and nvme-tcp setup

--- rocky9.4/worker_node.sh
+++ rocky9.4/worker_node.sh
@@ -2,6 +2,7 @@ echo "Setup Rocky OS huge page..."
 echo "vm.nr_hugepages = 4096" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
+
 echo "Setup nvme-tcp..."
 sudo modprobe nvme-tcp
 echo "nvme-tcp" | sudo tee /etc/modules-load.d/nvme-tcp.conf
